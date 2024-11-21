c67748b13e4fa3f5a3964524369d17d2665e033c
daniyakhan2005 <daniyadaniyakhan2@gmail.com>
2024-11-21 13:44:12 -0800

    navbar done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaSearch , FaShoppingCart, FaUser } from 'react-icons/fa'
+import logo  from '../assets/logo.jpeg'
+
+const Navbar = () => {
+  return (
+    <> 
+    <nav className='bg-white  shadow-md'>
+      <div className='container mx-auto px-4 md:px-16 lg:px-24 py-4 flex justify-between items-center'>
+        <div className= 'font-bold font-serif text-2xl'>
+          <Link to='/'>є-ѕнσρ</Link>
+
+        </div>
+        <div className='relative flex-1  mx-4'>  
+         <form>
+          <input type="text" placeholder='search product' className='w-full border py-2  px-4' />
+          <FaSearch className='absolute  top-3 right-3 text-red-500'></FaSearch>
+           
+         </form>
+        </div>
+        <div className='flex items-center space-x-4'>
+          <Link to='/cart'><FaShoppingCart className='text-lg'/>
+          </Link>
+          <button className='hidden md:block'>Login | Register</button>
+          <button className='block md:hidden'><FaUser/></button>
+        </div>
+      </div>
+        <div className='flex justify-center items-center space-x-10 py-4 text-sm font-bold'>
+        <Link to='/' className='hover:underline'>Home</Link>
+        <Link to='/' className='hover:underline'>Shop</Link>
+        <Link to='/' className='hover:underline'>Contact</Link>
+        <Link to='/' className='hover:underline'>About</Link>
+        </div>
+
+    </nav>
+    
+    </>
+   
+  )
+}
+
+export default Navbar
